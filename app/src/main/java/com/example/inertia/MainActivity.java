64eaa9bd94092d2d291f8861dc7dbe28ead679e3
ashwinadiga01 <ashwinadiga0111@gmail.com>
2021-10-11 14:54:19 +0530

    Splash screen added ✅

--- app/src/main/java/com/example/inertia/MainActivity.java
+++ app/src/main/java/com/example/inertia/MainActivity.java
@@ -2,13 +2,32 @@ package com.example.inertia;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button signOut;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        signOut = findViewById(R.id.signOut);
+
+        signOut.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                FirebaseAuth.getInstance().signOut();
+                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+
+                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);//makesure user cant go back
+                startActivity(intent);
+            }
+
+        });
+
     }
 }
---
+++ app/src/main/java/com/example/inertia/SplashScreen.java
@@ -0,0 +1,42 @@
+package com.example.inertia;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class SplashScreen extends AppCompatActivity {
+
+    FirebaseUser currentUser;
+    private FirebaseAuth mAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+        mAuth = FirebaseAuth.getInstance();
+        if (mAuth != null) {
+            currentUser = mAuth.getCurrentUser();
+        }
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                FirebaseUser user = mAuth.getCurrentUser();
+                if (user == null) {
+                    Intent intent = new Intent(SplashScreen.this, LoginActivity.class);
+                    startActivity(intent);
+                    finish();
+                } else {
+                    Intent mainIntent = new Intent(SplashScreen.this, MainActivity.class);
+                    mainIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                    startActivity(mainIntent);
+                    finish();
+                }
+            }
+        }, 1000);
+    }
+}
