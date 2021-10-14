fa7695d9f4b4c69fb5421cc65943db55cb3395a3
looping <53462249+nimit2801@users.noreply.github.com>
2021-10-14 19:45:46 +0530

    remove extra comments ✅

--- app/src/main/java/com/example/inertia/GoogleSignUp.java
+++ app/src/main/java/com/example/inertia/GoogleSignUp.java
@@ -17,13 +17,6 @@ public class GoogleSignUp {
     private FirebaseAuth mAuth;
     private Activity context;
 
-    // Check for existing Google Sign In account, if the user is already signed in
-    // the GoogleSignInAccount will be non-null.
-    // TODO:
-    // 1. Add toast to remind please login
-    // 2. Login with Google on Google Login Page
-//        updateUI(account);
-
     public GoogleSignUp(FirebaseAuth mAuth, Activity context, String idToken) {
         this.mAuth = mAuth;
         this.context = context;
