2f68202194e6ac39680d17688963429da65d20bb
nimit2801 <mnimitsavant@outlook.com>
2021-10-31 20:24:43 +0530

    Quick Fix: Redirect to Main Activity if Google Oauth Already exists. ✅

--- app/src/main/java/com/example/inertia/auth/GoogleSignUp.java
+++ app/src/main/java/com/example/inertia/auth/GoogleSignUp.java
@@ -5,6 +5,8 @@ import android.util.Log;
 
 import androidx.annotation.NonNull;
 
+import com.example.inertia.MainActivity;
+import com.example.inertia.helpers.RedirectToActivity;
 import com.example.inertia.helpers.StoreUserData;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
@@ -17,7 +19,8 @@ import com.google.firebase.auth.GoogleAuthProvider;
 public class GoogleSignUp {
     private FirebaseAuth mAuth;
     private Activity context;
-
+    // TODO:
+    // 1. Google Oauth not working accordingly (not important in Shreya's opinion)
     public GoogleSignUp(FirebaseAuth mAuth, Activity context, String idToken) {
         this.mAuth = mAuth;
         this.context = context;
@@ -29,7 +32,10 @@ public class GoogleSignUp {
                         if (task.isSuccessful()) {
                             Log.d("Success: ", "signInWithCredential:success");
                             FirebaseUser user = mAuth.getCurrentUser();
-                            new StoreUserData().redirectToEditProfile(context);
+                            if(user.getDisplayName() != null)
+                                new RedirectToActivity().redirectActivityAfterFinish(context, MainActivity.class);
+                            else
+                                new StoreUserData().redirectToEditProfile(context);
                         } else {
                             Log.w("Error: ", "signInWithCredential:failure", task.getException());
                         }
