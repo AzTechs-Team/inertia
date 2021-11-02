ec3f4bf50448706394edcce1f76cf9f458f0434c
unknown <srparadkar15@gmail.com>
2021-11-02 11:53:16 +0530

    Added card dialog in ProfileFragment ✅

--- app/src/main/java/com/example/inertia/profile/ProfileFeedTab.java
+++ app/src/main/java/com/example/inertia/profile/ProfileFeedTab.java
@@ -1,5 +1,8 @@
 package com.example.inertia.profile;
 
+import android.app.AlertDialog;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,11 +10,15 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.GridView;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.example.inertia.MainActivity;
 import com.example.inertia.R;
 import com.example.inertia.models.FeedImageModel;
+import com.squareup.picasso.Picasso;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -44,6 +51,29 @@ public class ProfileFeedTab extends Fragment {
         ProfileFeedGridViewAdapter adapter;
         adapter = new ProfileFeedGridViewAdapter(rootView.getContext(), feedPostsList);
         gridView.setAdapter(adapter);
+
+        gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                FeedImageModel item = (FeedImageModel) parent.getItemAtPosition(position);
+                LayoutInflater factory = LayoutInflater.from(getContext());
+                final View dialog = factory.inflate(R.layout.post_card_view, null);
+
+                ImageView img = dialog.findViewById(R.id.post_dialog_image);
+                TextView caption = dialog.findViewById(R.id.post_dialog_caption);
+                TextView location = dialog.findViewById(R.id.post_dialog_location);
+                Picasso.get().load(item.getImg()).into(img);
+
+                caption.setText(item.getCaption());
+                location.setText(item.getLocation());
+
+                final AlertDialog postDialog = new AlertDialog.Builder(getContext()).create();
+                postDialog.setView(dialog);
+                postDialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+                postDialog.show();
+            }
+        });
+
         return rootView;
     }
 }
