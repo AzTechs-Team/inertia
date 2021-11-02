3ca044707d56a9d1102f40de9f249550dfd7689c
ashwinadiga01 <ashwinadiga0111@gmail.com>
2021-11-02 19:15:42 +0530

    Added delete post feature ✅

--- app/src/main/java/com/example/inertia/helpers/StoreUserData.java
+++ app/src/main/java/com/example/inertia/helpers/StoreUserData.java
@@ -97,7 +97,8 @@ public class StoreUserData {
     public void addPostDataToFirebase(Activity context, Uri imageUri, String uid, String caption, String location){
             FirebaseStorage storage = FirebaseStorage.getInstance();
             StorageReference storageRef = storage.getReference();
-            final StorageReference ref = storageRef.child("images/" + uid + "/posts/"+ Timestamp.from(Instant.now()).getTime()+".jpg");
+            String timeInstance = String.valueOf(Timestamp.from(Instant.now()).getTime());
+            final StorageReference ref = storageRef.child("images/" + uid + "/posts/"+ timeInstance +".jpg");
             UploadTask uploadTask = ref.putFile(imageUri);
             Task<Uri> urlTask = uploadTask.continueWithTask(new Continuation<UploadTask.TaskSnapshot, Task<Uri>>() {
                 @Override
@@ -112,18 +113,17 @@ public class StoreUserData {
                 public void onComplete(@NonNull Task<Uri> task) {
                     if (task.isSuccessful()) {
                         Uri photoURI = task.getResult();
-                        storePostDetailsToFirestore(context, uid, photoURI, caption, location);
+                        storePostDetailsToFirestore(context, uid, photoURI, caption, location , timeInstance);
                     }
                 }
             });
     }
 
-    private void storePostDetailsToFirestore(Activity context, String uid, Uri photoURI , String caption, String location){
+    private void storePostDetailsToFirestore(Activity context, String uid, Uri photoURI , String caption, String location, String timeInstance){
         FirebaseFirestore db = FirebaseFirestore.getInstance();
-        String id = String.valueOf(Timestamp.from(Instant.now()).getTime());
 
         Map<String, Object> post = new HashMap<>();
-        post.put("id",id);
+        post.put("id",timeInstance);
         post.put("photoURI", photoURI.toString());
         post.put("caption",caption);
         post.put("location", location);
@@ -131,7 +131,7 @@ public class StoreUserData {
         db.collection("users")
                 .document(uid)
                 .collection("posts")
-                .document(id)
+                .document(timeInstance)
                 .set(post)
                 .addOnFailureListener(new OnFailureListener() {
                     @Override
--- app/src/main/java/com/example/inertia/profile/ProfileFeedTab.java
+++ app/src/main/java/com/example/inertia/profile/ProfileFeedTab.java
@@ -1,11 +1,13 @@
 package com.example.inertia.profile;
 
 import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
@@ -23,6 +25,11 @@ import com.example.inertia.MainActivity;
 import com.example.inertia.R;
 import com.example.inertia.models.FeedImageModel;
 import com.example.inertia.post.EditPostActivity;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
 import com.squareup.picasso.Picasso;
 
 import java.util.ArrayList;
@@ -93,7 +100,7 @@ public class ProfileFeedTab extends Fragment {
                                         break;
 
                                     case "Delete Post":
-                                        Toast.makeText(getContext(), "lmaooooooooooooooooo", Toast.LENGTH_SHORT).show();
+                                        deletePost(item.getId());
                                         break;
                                 }
                                 return true;
@@ -110,5 +117,55 @@ public class ProfileFeedTab extends Fragment {
         });
 
         return rootView;
+    }
+
+    private void deletePost(String pid){
+        FirebaseStorage storage = FirebaseStorage.getInstance();
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        AlertDialog.Builder deleteAlertBuilder = new AlertDialog.Builder(getContext());
+        deleteAlertBuilder.setMessage("Are you sure you want to delete this post?");
+        deleteAlertBuilder.setCancelable(true);
+
+        deleteAlertBuilder.setPositiveButton("Yes",
+            new DialogInterface.OnClickListener() {
+                public void onClick(DialogInterface dialog, int id) {
+                    db.collection("users")
+                        .document(MainActivity.userProfile.user.get("uid").toString())
+                        .collection("posts").document(pid)
+                        .delete()
+                        .addOnCompleteListener(new OnCompleteListener<Void>() {
+                            @Override
+                            public void onComplete(@NonNull Task<Void> task) {
+                                if (task.isSuccessful()) {
+                                    Toast.makeText(getContext(), "Post has been deleted.", Toast.LENGTH_SHORT).show();
+                                } else {
+                                    Toast.makeText(getContext(), "Failed to delete the post.", Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        });
+
+                    StorageReference storageRef = storage.getReference();
+                    StorageReference desertRef = storageRef.child("images/" + MainActivity.userProfile.user.get("uid").toString() + "/posts/" + pid + ".jpg");
+                    desertRef.delete();
+    //                          .addOnSuccessListener(new OnSuccessListener<Void>() {
+    //                            @Override
+    //                            public void onSuccess(Void aVoid) { }
+    //                        }).addOnFailureListener(new OnFailureListener() {
+    //                            @Override
+    //                            public void onFailure(@NonNull Exception exception) {
+    //                            }
+    //                        });
+                }
+            });
+
+        deleteAlertBuilder.setNegativeButton("No",
+            new DialogInterface.OnClickListener() {
+                public void onClick(DialogInterface dialog, int id) {
+                    dialog.cancel();
+                }
+            });
+
+        AlertDialog deleteAlert = deleteAlertBuilder.create();
+        deleteAlert.show();
     }
 }
