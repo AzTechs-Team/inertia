aae636ed9f9e32e042839e31c682c64b8484e2b1
nimit2801 <mnimitsavant@outlook.com>
2021-11-08 02:43:27 +0530

    Add: Persistance search Mock ✅

--- app/src/main/java/com/example/inertia/search/SearchFragment.java
+++ app/src/main/java/com/example/inertia/search/SearchFragment.java
@@ -1,5 +1,6 @@
 package com.example.inertia.search;
 
+import android.app.Activity;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,11 +8,23 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.SearchView;
 
 import com.example.inertia.R;
 
+import java.util.ArrayList;
+
 public class SearchFragment extends Fragment {
+
     public SearchFragment() { }
+
+    SearchView searchView;
+    ListView listView;
+
+    ArrayList<String> list;
+    ArrayAdapter<String> adapter;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -20,6 +33,29 @@ public class SearchFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_search, container, false);
+        View view = inflater.inflate(R.layout.fragment_search, container, false);
+        searchView = (SearchView) view.findViewById(R.id.search);
+        listView = (ListView) view.findViewById(R.id.search_items);
+
+        list = new ArrayList<String>();
+        list.add("Ashwin");
+        list.add("Nimit");
+        list.add("Shreya");
+
+        adapter = new ArrayAdapter<String>(view.getContext(), android.R.layout.simple_list_item_1, list);
+        listView.setAdapter(adapter);
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String s) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String s) {
+                adapter.getFilter().filter(s);
+                return false;
+            }
+        });
+        return view;
     }
 }
